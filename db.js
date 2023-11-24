1ac967ff243d32f8bffb206502fedeeea7d526f3
Anupam Singh <anupamsingh215@gmail.com>
2023-11-24 23:34:17 +0530

    dashboard apis updated

--- db.js
+++ db.js
@@ -1,10 +1,7 @@
 // db.js
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/star_bakery', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://localhost:27017/star_bakery");
 
 module.exports = mongoose;
--- models/branch.js
+++ models/branch.js
@@ -1,16 +1,13 @@
+const mongoose = require("../db");
+const branchSchema = new mongoose.Schema({
+  branchId: String,
+  name: String,
+  city: String,
+  noOfOrders: Number,
+  contact: String,
+  address: String,
+});
 
-  const mongoose = require("../db")
-  const branchSchema = new mongoose.Schema({
-    branchId: String,
-    name: String,
-    city: { type: String, default: "Bengaluru", require: true},
-    noOfOrders: Number,
-    contact: String,
-    address: String,
-  });
-
-  
-  // Create a mongoose model based on the schema
-   const Branch = mongoose.model('Branch', branchSchema);
-   module.exports = Branch
-  
+// Create a mongoose model based on the schema
+const Branch = mongoose.model("Branch", branchSchema);
+module.exports = Branch;
